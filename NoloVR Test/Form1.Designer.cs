6e4881169b6d07f9b3a6eddbfbeb13d65e0ee812
gb2111 <knurgb@gazeta.pl>
2018-07-05 09:04:56 +0200

    initial commit

---
+++ NoloVR Test/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NoloVR_Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.buttonConnect = new System.Windows.Forms.Button();
+			this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+			this.labelStatus = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonConnect
+			// 
+			this.buttonConnect.Location = new System.Drawing.Point(23, 12);
+			this.buttonConnect.Name = "buttonConnect";
+			this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+			this.buttonConnect.TabIndex = 0;
+			this.buttonConnect.Text = "Connect";
+			this.buttonConnect.UseVisualStyleBackColor = true;
+			this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+			// 
+			// timerUpdate
+			// 
+			this.timerUpdate.Enabled = true;
+			this.timerUpdate.Interval = 50;
+			this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+			// 
+			// labelStatus
+			// 
+			this.labelStatus.AutoSize = true;
+			this.labelStatus.Location = new System.Drawing.Point(20, 81);
+			this.labelStatus.Name = "labelStatus";
+			this.labelStatus.Size = new System.Drawing.Size(46, 13);
+			this.labelStatus.TabIndex = 1;
+			this.labelStatus.Text = "Status...";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(488, 445);
+			this.Controls.Add(this.labelStatus);
+			this.Controls.Add(this.buttonConnect);
+			this.Name = "Form1";
+			this.Text = "NOLO VR Test";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonConnect;
+		private System.Windows.Forms.Timer timerUpdate;
+		private System.Windows.Forms.Label labelStatus;
+	}
+}
+
---
+++ NoloVR Test/Form1.cs
@@ -0,0 +1,67 @@
+﻿using NOLO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoloVR_Test
+{
+	public partial class Form1 : Form
+	{
+		private static bool statusConnected = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonConnect_Click(object sender, EventArgs e)
+		{
+			funcCallback connectCallback = (context) => { connect_Nolo(context); };
+			funcCallback disconnectCallback = (context) => { disconnect_Nolo(context); };
+			noloDataNotifyFunc dataCallback = (noloData, context) => { noloDataNotify(noloData, context); };
+
+			NOLO.NOLOClient_V2_API.registerDisConnectCallBack(disconnectCallback, IntPtr.Zero);
+			NOLO.NOLOClient_V2_API.registerConnectSuccessCallBack(connectCallback, IntPtr.Zero);
+			NOLO.NOLOClient_V2_API.registerNoloDataNotifyCallBack(dataCallback, IntPtr.Zero);
+
+			NOLO.NOLOClient_V2_API.search_Nolo_Device();
+		}
+
+		private static void connect_Nolo(IntPtr context)
+		{
+			statusConnected = true;
+		}
+
+		private static void disconnect_Nolo(IntPtr context)
+		{
+			statusConnected = false;
+		}
+
+		private static void noloDataNotify(NOLO.NoloData noloData, IntPtr context)
+		{
+			//updateFPS.Update();
+		}
+
+		private void timerUpdate_Tick(object sender, EventArgs e)
+		{
+			string text = statusConnected ? "Connected" : "Not connected";
+
+			if (statusConnected)
+			{
+				NOLO.Controller leftController = NOLOClient_V2_API.get_Nolo_LeftControllerData();
+				NOLO.Controller rightController = NOLOClient_V2_API.get_Nolo_RightControllerData();
+
+				text += string.Format("\nLeft touchpad: {0:N2},{1:N2}", leftController.ControllerTouchAxis.x, leftController.ControllerTouchAxis.y);
+				text += string.Format("\nRight touchpad: {0:N2},{1:N2}", rightController.ControllerTouchAxis.x, rightController.ControllerTouchAxis.y);
+			}
+
+			labelStatus.Text = text;
+		}
+	}
+}
